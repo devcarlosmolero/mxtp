ba4c2b584050b2910f6c53fe7eb4e512a75f0aee
devcarlosmolero <dev@carlosmolero.com>
2025-12-21 22:38:07 +0100

    fix: enhance Bash dependency check in startup script
    - Introduce a more robust check for the presence of `$BASH_VERSION`.
    - Ensure that the script logs a fatal error if it's not running under Bash.
    - Improve the error message to include the detected Bash version.

--- lib/startup.sh
+++ lib/startup.sh
@@ -5,8 +5,11 @@ source "$MXTP_ROOT_DIR/lib/logger.sh"
 function check_dependency() {
     case $1 in
     "bash")
-        local bash_version=${BASH_VERSION:0:1}
-        if [[ $bash_version -lt 5 ]]; then log_fatal "Bash version must be at least 5"; fi
+        if [[ -z "$BASH_VERSION" ]]; then log_fatal "Not running under Bash"; fi
+        local major_version=${BASH_VERSION%%.*}
+        if ((major_version < 5)); then
+            log_fatal "Bash version must be at least 5 (found $BASH_VERSION)"
+        fi
         ;;
     "ffmpeg")
         command -v ffmpeg >/dev/null 2>&1 || { log_fatal "ffmpeg is required but not installed."; }
